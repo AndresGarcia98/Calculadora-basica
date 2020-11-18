de1d641d1598f720a50d02f3abfa40a574fb472a
Andres Garcia <garciaagudelod@gmail.com>
2020-11-18 17:26:06 -0500

    Se unen los tests

--- spec/jessica_test.spec.js
+++ spec/jessica_test.spec.js
@@ -35,6 +35,29 @@ describe('Mi test', ()=>{
         var resultado = principal.calculadora(-3, 0, '*');
         expect(resultado).toBe(0);
     });
+    it('Flujo 8', () => {
+        var resultado = principal.calculadora('r', 'g', '*');
+        expect(resultado).toBe('Deben de ser numeros');
+    });
+
+    it('Flujo 9', () => {
+        var resultado = principal.calculadora(-15, 3, '/');
+        expect(resultado).toBe(-5);
+    });
+
+    it('Flujo 10', () => {
+        var resultado = principal.calculadora(-15, -3, '/');
+        expect(resultado).toBe(5);
+    });
+    it('Flujo 11', () => {
+        var resultado = principal.calculadora(-3, 0, '/');
+        expect(resultado).toBe('No se permite division por cero');
+    });
+
+    it('Flujo 12', () => {
+        var resultado = principal.calculadora(0, 5, '/');
+        expect(resultado).toBe(0);
+    });
 });
 
 
