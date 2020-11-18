74384acf1dc6568c366b542f020359e98544b012
Andres Garcia <garciaagudelod@gmail.com>
2020-11-18 17:24:52 -0500

    Update jessica_test.spec.js

--- spec/jessica_test.spec.js
+++ spec/jessica_test.spec.js
@@ -16,6 +16,25 @@ describe('Mi test', ()=>{
         var resultado = principal.calculadora(3, -5, '-');
         expect(resultado).toBe(8);
     });
+    
+     it('Flujo 4', ()=>{
+        var resultado = principal.calculadora(3, 5, '*');
+        expect(resultado).toBe(15);
+    });
+
+    it('Flujo 5', ()=>{
+        var resultado = principal.calculadora(-3, 5, '*');
+        expect(resultado).toBe(-15);
+    });
+    
+    it('Flujo 6', ()=>{
+        var resultado = principal.calculadora(-3, -5, '*');
+        expect(resultado).toBe(15);
+    });
+    it('Flujo 7', ()=>{
+        var resultado = principal.calculadora(-3, 0, '*');
+        expect(resultado).toBe(0);
+    });
 });
 
 
